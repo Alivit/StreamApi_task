7e5a03191baa37e475403a733c6d2375ca593d8d
Alivit <vltal241201@gmail.com>
2023-02-14 23:33:21 +0300

    feat: add release task15

--- src/main/java/by/chuiko/Main.java
+++ src/main/java/by/chuiko/Main.java
@@ -1,7 +1,6 @@
 package by.chuiko;
 
 import by.chuiko.model.Animal;
-import by.chuiko.model.Car;
 import by.chuiko.model.Flower;
 import by.chuiko.model.House;
 import by.chuiko.model.Person;
@@ -10,6 +9,7 @@ import by.chuiko.util.Util;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Stream;
@@ -19,7 +19,7 @@ import static java.util.Comparator.comparing;
 public class Main {
     public static void main(String[] args) throws IOException {
         //task1();
-        task2();
+        //task2();
         //task3();
         //task4();
         //task5();
@@ -30,8 +30,8 @@ public class Main {
         //task10();
         //task11();
         //task12();
-        task13();
-        task14();
+        //task13();
+        //task14();
         task15();
     }
 
@@ -132,17 +132,42 @@ public class Main {
     }
 
     private static void task13() throws IOException {
-        List<House> houses = Util.getHouses();
-        //        Продолжить...
+//        List<House> houses = Util.getHouses();
+//        Stream<Stream<Person>> people1 = houses.stream()
+//                .filter(house -> "Hospital".equals(house.getBuildingType()))
+//                .map(house -> house.getPersonList().stream());
+//        Stream<Stream<Person>> people2 = houses.stream()
+//                .filter(house -> "Civil building".equals(house.getBuildingType()))
+//                .map(house -> house.getPersonList().stream());
+//        people2.map(personStream -> personStream
+//                        .filter(person -> ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.of(2023, 2, 14)) <= 18))
+//                .forEach(person -> person.forEach(System.out::println));
+//        Stream<Person> peopleNew = people1;
+//        Stream.concat(people1,people2).forEach(personStream1 -> personStream1.forEach(System.out::println));
+
+
     }
 
     private static void task14() throws IOException {
-        List<Car> cars = Util.getCars();
-        //        Продолжить...
+//        List<Car> cars = Util.getCars();
+//        Stream<Car> turk = cars.stream()
+//                .filter(car -> "Jaguar".equals(car.getCarMake()) || car.getColor().equals("White"));
+//        turk.forEach(System.out::println);
+//        Stream<Car> usb = cars.stream()
+//                .filter(car -> !"Jaguar".equals(car.getCarMake()) && car.getColor().equals("White"))
+//                .filter(car -> car.getMass() <= 1500 || car.getCarMake().equals("BMW"))
+
     }
 
     private static void task15() throws IOException {
         List<Flower> flowers = Util.getFlowers();
-        //        Продолжить...
+        System.out.println("Ответ: " + flowers.stream()
+                .sorted(comparing(Flower::getOrigin).reversed().thenComparing(Flower::getPrice).reversed()
+                        .thenComparing(Flower::getWaterConsumptionPerDay).reversed())
+                .filter(flower -> flower.getCommonName().charAt(0) >= 'C' && flower.getCommonName().charAt(0) <= 'S')
+                .filter(flower -> flower.isShadePreferred() && (flower.getFlowerVaseMaterial().stream()
+                        .anyMatch(o -> "Glass".equals(o) || "Aluminum".equals(o) || "Steel".equals(o))))
+                .mapToDouble(flower -> flower.getPrice() + ((flower.getWaterConsumptionPerDay() * 365 * 5)/1000 * 1.39)).sum());
+
     }
 }
